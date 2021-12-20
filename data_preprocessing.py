941154f42cc573f9bbe7d4cf9cc6c9fd71598c55
Lavender322 <yiningyuan322@gmail.com>
2021-12-20 22:47:53 +0000

    Reorganised the project files

--- data_preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Create Task A label
-df = pd.read_csv("/dataset/label.csv")
-df.loc[df["label"]=="meningioma_tumor", "label"] = "tumor"
-df.loc[df["label"]=="glioma_tumor", "label"] = "tumor"
-df.loc[df["label"]=="pituitary_tumor", "label"] = "tumor"
-df.to_csv("/dataset/label_task_A.csv", index=False)
